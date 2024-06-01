884c388bb5319f3fd63adda0303d77762c975bd2
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-01 15:14:39 +0200

    Update Smartmail-assistant-dev.php

--- Smartmail-assistant-dev.php
+++ Smartmail-assistant-dev.php
@@ -3,7 +3,7 @@
  * Plugin Name: SmartMail Assistant Developer
  * Description: Developer version of SmartMail Assistant for managing subscriptions and API integrations.
  * Version: 1.0.0
- * Author: Your Name
+ * Author: Marco Zagato
  */
 
 // Prevent direct access
