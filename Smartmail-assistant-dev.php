0a1b1a345ee9a18dc670042b1fad5a8ad47d0510
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-03 01:15:34 +0200

    Update Smartmail-assistant-dev.php

--- Smartmail-assistant-dev.php
+++ Smartmail-assistant-dev.php
@@ -3,7 +3,7 @@
 Plugin Name: SmartMail Assistant Dev
 Description: Developer plugin for SmartMail Assistant
 Version: 1.0
-Author: Marco Zagato
+Author: Marco Z
 Author URI: https://smartmail.store
 */
 
