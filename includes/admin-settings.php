4377ce3e444725a0b5ea7fda4eb3e7217b2061a6
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-04 20:01:09 +0200

    Update admin-settings.php

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -38,4 +38,16 @@ function smartmail_admin_page() {
 function smartmail_register_settings() {
     register_setting('smartmail_options_group', 'smartmail_option_name');
     add_settings_section('smartmail_main_section', 'Main Settings', 'smartmail_main_section_cb', 'smartmail');
-    add_settings_field('smartmail​⬤
+    add_settings_field('smartmail_option_name', 'Option Name', 'smartmail_option_name_cb', 'smartmail', 'smartmail_main_section');
+}
+add_action('admin_init', 'smartmail_register_settings');
+
+function smartmail_main_section_cb() {
+    echo '<p>Main description of this section here.</p>';
+}
+
+function smartmail_option_name_cb() {
+    $setting = get_option('smartmail_option_name');
+    echo "<input type='text' name='smartmail_option_name' value='" . esc_attr($setting) . "'>";
+}
+?>
